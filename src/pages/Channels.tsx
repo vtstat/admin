ea94f763902268c1f1006e304b7f82adbf2203c5
PoiScript <poiscript@gmail.com>
2023-07-16 19:18:00 +0800

    feat(channels): show channel platform also

--- src/pages/Channels.tsx
+++ src/pages/Channels.tsx
@@ -1,6 +1,7 @@
 import {
   Table,
   TableContainer,
+  Tag,
   Tbody,
   Td,
   Th,
@@ -12,15 +13,14 @@ import { useQuery } from "react-query";
 
 import { fetch } from "../utils/fetch";
 
-type Props = {};
-
 type Channel = {
   channel_id: number;
   platform_id: string;
   vtuber_id: string;
+  platform: string;
 };
 
-const Channels: React.FC<Props> = ({}) => {
+const Channels: React.FC = () => {
   const { data: channels = [] } = useQuery(["channels"], () =>
     fetch<Channel[]>("/channels")
   );
@@ -31,22 +31,38 @@ const Channels: React.FC<Props> = ({}) => {
         <Thead position="sticky" top="60px" zIndex={1000} bgColor="white">
           <Tr>
             <Th isNumeric>ID</Th>
-            <Th>Platform Id</Th>
             <Th>VTuber Id</Th>
+            <Th>Platform</Th>
+            <Th>Platform Id</Th>
           </Tr>
         </Thead>
         <Tbody>
           {channels.map((job) => (
             <Tr key={job.channel_id}>
               <Td isNumeric>{job.channel_id}</Td>
+              <Td isNumeric>{job.vtuber_id}</Td>
+              <Td>
+                <PlatformTag>{job.platform}</PlatformTag>
+              </Td>
               <Td>{job.platform_id}</Td>
-              <Td>{job.vtuber_id}</Td>
             </Tr>
           ))}
         </Tbody>
       </Table>
     </TableContainer>
   );
+};
+
+const PlatformTag: React.FC<{ children: string }> = ({ children }) => {
+  switch (children) {
+    case "Youtube": {
+      return <Tag colorScheme="red">YouTube</Tag>;
+    }
+
+    default: {
+      return <Tag>{children}</Tag>;
+    }
+  }
 };
 
 export default Channels;
