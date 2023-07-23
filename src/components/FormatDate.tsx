1591c283849f1100065b749c0da51ca88f07685c
PoiScript <poiscript@gmail.com>
2023-07-23 16:10:53 +0800

    feat: add realtive date format

--- src/components/FormatDate.tsx
+++ src/components/FormatDate.tsx
@@ -1,21 +1,43 @@
 import { Tooltip } from "@chakra-ui/react";
-import { format, formatDistanceToNowStrict, parseISO } from "date-fns";
+import {
+  format,
+  formatDistanceToNowStrict,
+  isThisWeek,
+  isThisYear,
+  isToday,
+  isTomorrow,
+  isYesterday,
+  parseISO,
+} from "date-fns";
 import React from "react";
 
 type Props = {
   children: Date | string | number | null | undefined;
 };
 
-const FormatDate: React.FC<Props> = ({ children }) => {
-  if (!children) return null;
+const FormatDate: React.FC<Props> = ({ children: date }) => {
+  if (!date) return null;
 
-  if (typeof children === "string") children = parseISO(children);
+  if (typeof date === "string") date = parseISO(date);
 
-  return (
-    <Tooltip label={formatDistanceToNowStrict(children, { addSuffix: true })}>
-      {format(children, "MM-dd HH:mm, E")}
-    </Tooltip>
-  );
+  const duration = formatDistanceToNowStrict(date, { addSuffix: true });
+
+  let msg;
+  if (isToday(date)) {
+    msg = format(date, "'Today' HH:mm");
+  } else if (isYesterday(date)) {
+    msg = format(date, "'Yesterday' HH:mm");
+  } else if (isTomorrow(date)) {
+    msg = format(date, "'Tomorrow' HH:mm");
+  } else if (isThisWeek(date)) {
+    msg = format(date, "E HH:mm");
+  } else if (isThisYear(date)) {
+    msg = format(date, "MM-dd HH:mm");
+  } else {
+    msg = format(date, "yyyy-MM-dd HH:mm");
+  }
+
+  return <Tooltip label={duration}>{msg}</Tooltip>;
 };
 
 export default FormatDate;
